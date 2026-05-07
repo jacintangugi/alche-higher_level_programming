4e24d95835111e31fddd88d09f9776fd1a5a6b16
jacintangugi <j.ngugi@alustudent.com>
2026-05-07 01:19:12 -0700

    fix PEP8 in 4-inherits_from.py

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -3,5 +3,7 @@
 
 
 def inherits_from(obj, a_class):
-    """Return True if obj is an instance of a class that inherited from a_class."""
+    """Return True if obj is an instance of a class that inherited
+    from a_class, but not an instance of a_class itself.
+    """
     return isinstance(obj, a_class) and type(obj) is not a_class
